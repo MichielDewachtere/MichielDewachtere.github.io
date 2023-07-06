74ab310b2b7c24e9904c5b6a3017666cc1fce859
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-07-06 20:30:40 +0200

    Scroll link functionallity

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ menu.onclick = () => {
     menu.classList.toggle("bx-x");
     navList.classList.toggle("open");
 }
+
+const projectsAnchor = document.querySelector('a[href="#projects"]');
+const projectTargetSection = document.querySelector('#projects');
+
+projectsAnchor.addEventListener('click', function (event) {
+  event.preventDefault(); // Prevent default link behavior
+  projectTargetSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+const contactAnchor = document.querySelector('a[href="#contact"]');
+const contactTargetSection = document.querySelector('#contact');
+
+contactAnchor.addEventListener('click', function (event) {
+  event.preventDefault(); // Prevent default link behavior
+  contactTargetSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+const aboutAnchor = document.querySelector('a[href="#about"]');
+const aboutTargetSection = document.querySelector('#about');
+
+aboutAnchor.addEventListener('click', function (event) {
+  event.preventDefault(); // Prevent default link behavior
+  aboutTargetSection.scrollIntoView({ behavior: 'smooth' });
+});
