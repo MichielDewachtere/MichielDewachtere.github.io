9b6c8c38a2d5de716397eeebcf797793f1f849cd
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-12-31 14:36:14 +0100

    Moved smooth scrolling logic from css to javascript. Worked fine on FireFox but not on Chrome. With javascript method no issues on either platform.

---
+++ smooth-scrolling.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const options = {
+        behavior: 'smooth',
+        block: 'start',
+    };
+
+    sections.forEach((section, index) => {
+        section.addEventListener('wheel', (event) => {
+            if (event.deltaY > 0 && index < sections.length - 1) {
+                // Scroll down
+                sections[index + 1].scrollIntoView(options);
+            } else if (event.deltaY < 0 && index > 0) {
+                // Scroll up
+                sections[index - 1].scrollIntoView(options);
+            }
+        });
+    });
+});
